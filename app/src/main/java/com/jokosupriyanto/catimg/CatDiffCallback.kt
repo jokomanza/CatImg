367c48ded08d2b2122d9d342b9492782c63d67ce
Joko Supriyanto <jokomanza@gmail.com>
2021-06-03 10:37:16 +0700

    create cat diff callback

---
+++ app/src/main/java/com/jokosupriyanto/catimg/CatDiffCallback.kt
@@ -0,0 +1,26 @@
+package com.jokosupriyanto.catimg
+
+import androidx.recyclerview.widget.DiffUtil
+
+class CatDiffCallback (
+    private val old: List<Cat>,
+    private val new: List<Cat>
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int {
+        return old.size
+    }
+
+    override fun getNewListSize(): Int {
+        return new.size
+    }
+
+    override fun areItemsTheSame(oldPosition: Int, newPosition: Int): Boolean {
+        return old[oldPosition].id == new[newPosition].id
+    }
+
+    override fun areContentsTheSame(oldPosition: Int, newPosition: Int): Boolean {
+        return old[oldPosition] == new[newPosition]
+    }
+
+}
