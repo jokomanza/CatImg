a0b28512295f88401d902e6de1c40bea8034d6e2
Joko Supriyanto <jokomanza@gmail.com>
2021-06-03 10:36:18 +0700

    create api service

---
+++ app/src/main/java/com/jokosupriyanto/catimg/ApiService.kt
@@ -0,0 +1,19 @@
+package com.jokosupriyanto.catimg
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiService {
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://thatcopy.pw/catapi/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
+}
